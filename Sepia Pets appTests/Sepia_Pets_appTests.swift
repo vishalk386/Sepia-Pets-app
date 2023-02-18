34c33047031847b51223030b196fd43236f57b70
Vishal Kamble <vishalkamble8939@gmail.com>
2023-02-18 09:49:59 +0530

    Initial Commit

---
+++ Sepia Pets appTests/Sepia_Pets_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sepia_Pets_appTests.swift
+//  Sepia Pets appTests
+//
+//  Created by Vishal Kamble on 18/02/23.
+//
+
+import XCTest
+@testable import Sepia_Pets_app
+
+final class Sepia_Pets_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
